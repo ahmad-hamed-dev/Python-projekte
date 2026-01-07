5bb68b1f49464049ebc67d45836431c2208d154b
ahmad-hamed-dev <ahmadhamedof@gmail.com>
2026-01-07 01:02:36 +0100

    Delete wie_alt_bist_du.py

--- wie_alt_bist_du.py
+++
@@ -1,9 +0,0 @@
-import random
-rand= random.randint(10000,99999)
-zahl= int(input(" das passwort besteht aus fünf zahlen, kannst du das passwort erraten? \n"))
-if rand==zahl:
- print ("das paswort ist richtig")
-elif len(str(zahl))!=5:
- print("das passwort besteht aus nur fünf zahlen!")
-else:
- print(f"leider isr das passwort {rand}")
